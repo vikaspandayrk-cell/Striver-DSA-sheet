cdaa13158aaa3e6f294a3606e64538b7bf200e60
vikas pandey <vikas.panday.rk@gmail.com>
2026-01-07 17:37:03 +0530

    Added input output  program in 00_basics folder

---
+++ 00_basics/inputoutput.java
@@ -0,0 +1,7 @@
+class Solution {
+    static void printIntString(String S, int N) {
+    System.out.println("The input string :" +S);
+    System.out.println("The input integer :" +N);
+        
+    }
+}
